3571f3cf2247f63b9e27676edc660cba90260a90
Ashwin Goyal <ashgo1995@gmail.com>
2019-03-01 03:45:46 -0500

    The generation of node number has been updated
    The generation of a unique number corresponding to each state has been updated. It has been made more intuitive to understand.

--- code/nodeNumber.m
+++ code/nodeNumber.m
@@ -1,11 +1,9 @@
-function [ state ] = nodeNumber( node )
-
+function state = nodeNumber(node)
 % This function converts the node into state, that is, it assigns a unique 
 % number to the given node
 
-node = reshape(node',[1,9]);
-nodeConverter = [9^0; 9^1; 9^2; 9^3; 9^4; 9^5; 9^6; 9^7; 9^8];
+node = reshape(node', [1, 9]);
+nodeConverter = [10^8; 10^7; 10^6; 10^5; 10^4; 10^3; 10^2; 10^1; 10^0];
 state = node*nodeConverter;
 
 end
-
