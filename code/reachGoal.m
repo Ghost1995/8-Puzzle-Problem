d6b1372256aa5430269f9ee97e28d6890109ebd7
Ashwin Goyal <ashgo1995@gmail.com>
2019-03-03 19:39:39 -0500

    Update reachGoal.m
    The minor changes in the code are made to improve performance by pre-allocating the size of variables

--- code/reachGoal.m
+++ code/reachGoal.m
@@ -1,81 +1,64 @@
-function nodePath = reachGoal(NodeSet, saveVar, saveType)
+function nodePath = reachGoal(NodeSet)
 % This function reaches the goal node from the initial node
 
-% Check if all variables were defined correctly
-if nargin == 0
-    disp('NodeSet not defined');
-    nodePath = [];
-    return;
-elseif ~checkVars(NodeSet)
-    nodePath = [];
-    return;
-elseif nargin == 1 || ~islogical(saveVar)
-    saveVar = false;
-elseif nargin == 2 || (saveVar && ~strcmp(saveType,'.mat') && ~strcmp(saveType,'.txt'))
-    saveType = '.mat';
-end
-
-% Initialize all variables
-nodePath = NodeSet.Nodes(:, :, end);
-parentNode = NodeSet.NodesInfo(NodeSet.NodesInfo(:, 1) == nodeNumber(nodePath), 2);
-
-% Start developing the path
-while parentNode ~= 0
-    parentNodePos = find(NodeSet.NodesInfo(:, 1) == parentNode);
-    nodePath = cat(3, nodePath, NodeSet.Nodes(:, :, parentNodePos));
-    parentNode = NodeSet.NodesInfo(parentNodePos, 2);
-end
-nodePath = flip(nodePath, 3);
-
-% Save variables, if asked
-if saveVar
-    if strcmp(saveType,'.mat')
-        save ../output/nodePath.mat nodePath
-    elseif strcmp(saveType,'.txt')
-        path = permute(nodePath, [2 1 3]);
-        path = reshape(path, 9, [])';
-        if ispc || ismac
-            dlmwrite('../output/nodePath.txt', path, 'delimiter', '\t', 'newline','pc')
-        elseif isunix
-             dlmwrite('../output/nodePath.txt', path, 'delimiter', '\t', 'newline','unix')
-        end
+    % Check if all variables were defined correctly
+    if nargin == 0
+        disp('NodeSet not defined');
+        nodePath = [];
+        return;
+    elseif ~checkVars(NodeSet)
+        nodePath = [];
+        return;
     end
-end
+
+    % Initialize all variables
+    nodePath = zeros(3, 3, NodeSet.NodesInfo(end,3) + 1);
+    nodePath(:,:,end) = NodeSet.Nodes(:,:,end);
+    parentNodePos = NodeSet.NodesInfo(end,2);
+
+    % Start developing the path
+    for i = size(nodePath,3)-1:-1:1
+        nodePath(:,:,i) = NodeSet.Nodes(:,:,parentNodePos);
+        parentNodePos = NodeSet.NodesInfo(parentNodePos,2);
+    end
 
 end
 
 function status = checkVars(NodeSet)
 % This function checks for the validity of the inputs
 
-if isempty(NodeSet)
-    disp('NodeSet is empty');
-    status = false;
-elseif ~isstruct(NodeSet)
-    disp('NodeSet is not a structure');
-    status = false;
-elseif ~isfield(NodeSet, 'Nodes')
-    disp('NodeSet does not have Nodes field');
-    status = false;
-elseif isempty(NodeSet.Nodes)
-    disp('Nodes field of NodeSet is empty');
-    status = false;
-elseif (size(NodeSet.Nodes, 1) ~= 3) || (size(NodeSet.Nodes, 2) ~= 3)
-    disp('The size of Nodes field of NodeSet should be 3 x 3 x N');
-    status = false;
-elseif ~isfield(NodeSet, 'NodesInfo')
-    disp('NodeSet does not have NodesInfo field');
-    status = false;
-elseif isempty(NodeSet.NodesInfo)
-    disp('NodesInfo field of NodeSet is empty');
-    status = false;
-elseif size(NodeSet.NodesInfo, 2) ~= 3
-    disp('The size of NodesInfo field of NodeSet should be N x 3');
-    status = false;
-elseif size(NodeSet.NodesInfo, 1) ~= size(NodeSet.Nodes, 3)
-    disp('Nodes field of NodeSet and NodesInfo field of NodeSet are not of the same size');
-    status = false;
-else
-    status = true;
-end
+    if isempty(NodeSet)
+        disp('NodeSet is empty');
+        status = false;
+    elseif ~isstruct(NodeSet)
+        disp('NodeSet is not a structure');
+        status = false;
+    elseif ~isfield(NodeSet, 'Nodes')
+        disp('NodeSet does not have Nodes field');
+        status = false;
+    elseif isempty(NodeSet.Nodes)
+        disp('Nodes field of NodeSet is empty');
+        status = false;
+    elseif (size(NodeSet.Nodes, 1) ~= 3) || (size(NodeSet.Nodes, 2) ~= 3)
+        disp('The size of Nodes field of NodeSet should be 3 x 3 x N');
+        status = false;
+    elseif ~isfield(NodeSet, 'NodesInfo')
+        disp('NodeSet does not have NodesInfo field');
+        status = false;
+    elseif isempty(NodeSet.NodesInfo)
+        disp('NodesInfo field of NodeSet is empty');
+        status = false;
+    elseif size(NodeSet.NodesInfo, 2) ~= 3
+        disp('The size of NodesInfo field of NodeSet should be N x 3');
+        status = false;
+    elseif size(NodeSet.NodesInfo, 1) ~= size(NodeSet.Nodes, 3)
+        disp('Nodes field of NodeSet and NodesInfo field of NodeSet are not of the same size');
+        status = false;
+    elseif isfield(NodeSet, 'GoalReached') && islogical(NodeSet.GoalReached) && ~NodeSet.GoalReached
+        disp('NodeSet does not have the goal node');
+        status = false;
+    else
+        status = true;
+    end
 
 end
