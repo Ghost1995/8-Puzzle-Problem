c72190130ef98c2b535a417563b9b9b6ca809ecb
Ashwin Goyal <ashgo1995@gmail.com>
2019-03-03 19:38:49 -0500

    Update nodesGenerator.m
    This function is modified to work with maps in order to improve the computational time. Now, the algorithm is approximately 10 times faster. This is because no comparison is done for nodeNumber every time a new node is added.

--- code/nodesGenerator.m
+++ code/nodesGenerator.m
@@ -1,105 +1,75 @@
-function NodeSet = nodesGenerator(initialNode, goalNode, checkSolvability, saveVar, saveType)
+function NodeSet = nodesGenerator(initialNode, goalNode)
 % This function takes as input the initial node. The output of the
 % function is a struct containing all possible nodes from initial node to
 % the goal node.
 
-% Check if all variables were defined
-if nargin == 0
-    disp('Initial Node not defined');
-    NodeSet = [];
-    return;
-elseif nargin == 1
-    goalNode = [1 2 3; 4 5 6; 7 8 0];
-    checkSolvability = false;
-    saveVar = false;
-elseif nargin == 2
-    checkSolvability = false;
-    saveVar = false;
-elseif nargin == 3 || ~islogical(saveVar)
-    saveVar = false;
-elseif nargin == 4 || (saveVar && ~strcmp(saveType,'.mat') && ~strcmp(saveType,'.txt'))
-    saveType = '.mat';
-end
-
-% Check if all variables were correctly defined
-if isempty(initialNode)
-    disp('Initial Node is empty');
-    NodeSet = [];
-    return;
-elseif any(size(initialNode) ~= [3 3])
-    disp('The size of Initial Node should be 3 x 3');
-    NodeSet = [];
-    return;
-elseif isempty(goalNode) || any(size(goalNode) ~= [3 3])
-    goalNode = [1 2 3; 4 5 6; 7 8 0];
-end
-
-% Check solvability, if asked
-if islogical(checkSolvability) && checkSolvability && ~isSolvable(initialNode, goalNode)
-    disp('The given set of initial node and goal node does not have a solution')
-    NodeSet = [];
-    return;
-end
-
-% Initialize all variables
-Nodes = initialNode;
-NodesInfo = [nodeNumber(Nodes(:,:,1)), 0, 0]; % NodesInfo = [Node #, Parent node #, CostToCome]
-movement = {'up', 'right', 'down', 'left'};
-goalNodeNum = nodeNumber(goalNode);
-
-% Start exploration
-index = 0;
-oldCost = 0;
-goalReached = false;
-while ~goalReached && (index < size(Nodes, 3))
-    index = index + 1;
-    currentNode = Nodes(:, :, index);
-    c2c = NodesInfo(index, 3) + 1;
-    if c2c > oldCost
-        disp(['Currently at Layer ' num2str(c2c) ' of the Search Tree']);
-        oldCost = c2c;
+    % Check if all variables were defined
+    if nargin == 0
+        disp('Initial Node not defined');
+        NodeSet = [];
+        return;
+    elseif nargin == 1
+        goalNode = [1 2 3; 4 5 6; 7 8 0];
     end
-    % Move the blank space
-    for i = 1:4
-        [newNode, status] = moveBlankTile(currentNode, movement{i});
-        if status && ~any(NodesInfo(:, 1) == nodeNumber(newNode))
-            newNodeNum = nodeNumber(newNode);
-            Nodes = cat(3, Nodes, newNode);
-            NodesInfo(size(Nodes, 3), :) = [newNodeNum, NodesInfo(index, 1), c2c];
-            if newNodeNum == goalNodeNum
-                goalReached = true;
-                break;
+
+    % Check if all variables were correctly defined
+    if isempty(initialNode)
+        disp('Initial Node is empty');
+        NodeSet = [];
+        return;
+    elseif any(size(initialNode) ~= [3 3])
+        disp('The size of Initial Node should be 3 x 3');
+        NodeSet = [];
+        return;
+    elseif isempty(goalNode) || any(size(goalNode) ~= [3 3])
+        goalNode = [1 2 3; 4 5 6; 7 8 0];
+    end
+
+    % Initialize all variables
+    Nodes = zeros(3, 3, factorial(9));
+    Nodes(:,:,1) = initialNode;
+    NodesInfo = zeros(factorial(9), 3); % NodesInfo = [Node #, Parent node #, CostToCome]
+    NodesInfo(1,:) = [1 0 0];
+    NodesNumList = containers.Map(nodeNumber(initialNode), 1);
+    goalNodeNum = nodeNumber(goalNode);
+
+    % Start exploration
+    currNodeIndex = 0;
+    newNodeIndex = 1;
+    oldCost = 0;
+    while currNodeIndex < NodesNumList.Count
+        currNodeIndex = currNodeIndex + 1;
+        c2c = NodesInfo(currNodeIndex,3) + 1;
+        if c2c > oldCost
+            disp(['Currently at Layer ' num2str(c2c) ' of the Search Tree']);
+            oldCost = c2c;
+        end
+        % Move the blank space
+        [status, newNode, newNodeNum] = moveBlankTile(Nodes(:,:,currNodeIndex));
+        for i = 1:4
+            if status(i) && ~NodesNumList.isKey(newNodeNum(i))
+                newNodeIndex = newNodeIndex + 1;
+                NodesNumList(newNodeNum(i)) = newNodeIndex;
+                Nodes(:,:,newNodeIndex) = newNode(:,:,i);
+                NodesInfo(newNodeIndex,:) = [newNodeIndex, currNodeIndex, c2c];
+                if newNodeNum(i) == goalNodeNum
+                    currNodeIndex = inf;
+                    break;
+                end
             end
         end
     end
-end
 
-% Check if the goal was reached
-if ~goalReached
-    disp('The given set of initial node and goal node does not have a solution')
-end
-
-% Create struct
-NodeSet.Nodes = Nodes;
-NodeSet.NodesInfo = NodesInfo;
-
-% Save variables, if asked
-if saveVar
-    if strcmp(saveType,'.mat')
-        save ../output/NodeSet.mat NodeSet
-    elseif strcmp(saveType,'.txt')
-        nodes = permute(NodeSet.Nodes, [2 1 3]);
-        nodes = reshape(nodes, 9, [])';
-        fileID = fopen('../output/NodesInfo.txt','w');
-        if ispc || ismac
-            dlmwrite('../output/Nodes.txt', nodes, 'delimiter', '\t', 'newline','pc')
-            fprintf(fileID,'%09d\t%09d\t%d\r\n',NodeSet.NodesInfo');
-        elseif isunix
-            dlmwrite('../output/Nodes.txt', nodes, 'delimiter', '\t', 'newline','unix')
-            fprintf(fileID,'%09d\t%09d\t%d\n',NodeSet.NodesInfo');
-        end
-        fclose(fileID);
+    % Create struct
+    if NodesNumList.isKey(goalNodeNum)
+        goalNodePos = NodesNumList(goalNodeNum);
+        NodeSet.Nodes = Nodes(:,:,1:goalNodePos);
+        NodeSet.NodesInfo = NodesInfo(1:goalNodePos,:);
+        NodeSet.GoalReached = true;
+    else
+        NodeSet.Nodes = Nodes(:,:,1:newNodeIndex);
+        NodeSet.NodesInfo = NodesInfo(1:newNodeIndex,:);
+        NodeSet.GoalReached = false;
     end
-end
 
 end
