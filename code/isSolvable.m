f5ab5ba4e62a22eb5aded1c1903d3e6f06e4eac1
Ashwin Goyal <ashgo1995@gmail.com>
2019-03-03 19:35:23 -0500

    Modified indentation

--- code/isSolvable.m
+++ code/isSolvable.m
@@ -2,25 +2,26 @@ function status = isSolvable(initialNode, goalNode)
 % This function checks whether the given initial node can reach the given
 % goal node or not. If not, the function returns a false status.
 
-% Reshape the matrices into row matrices
-initialNode = reshape(initialNode',[1 9]);
-goalNode = reshape(goalNode',[1 9]);
+    % Reshape the matrices into row matrices
+    initialNode = reshape(initialNode',[1 9]);
+    goalNode = reshape(goalNode',[1 9]);
 
-% Count the number of inversions
-count = 0;
-for i = 1:8
-    for j = i+1:9
-        if all(initialNode([i,j]) ~= 0) && (find(goalNode == initialNode(i)) > find(goalNode == initialNode(j)))
-            count = count + 1;
+    % Count the number of inversions
+    count = 0;
+    for i = 1:8
+        for j = i+1:9
+            if all(initialNode([i,j]) ~= 0) &&...
+                    (find(goalNode == initialNode(i)) > find(goalNode == initialNode(j)))
+                count = count + 1;
+            end
         end
     end
-end
 
-% Check if the number of inversions are even or not
-if rem(count,2) ~= 0
-    status = false;
-else
-    status = true;
-end
+    % Check if the number of inversions are even or not
+    if rem(count,2) ~= 0
+        status = false;
+    else
+        status = true;
+    end
 
 end
--- code/nodeNumber.m
+++ code/nodeNumber.m
@@ -2,8 +2,8 @@ function state = nodeNumber(node)
 % This function converts the node into state, that is, it assigns a unique 
 % number to the given node
 
-node = reshape(node', [1, 9]);
-nodeConverter = [10^8; 10^7; 10^6; 10^5; 10^4; 10^3; 10^2; 10^1; 10^0];
-state = node*nodeConverter;
+    node = reshape(node', [1, 9]);
+    nodeConverter = [10^8; 10^7; 10^6; 10^5; 10^4; 10^3; 10^2; 10^1; 10^0];
+    state = node*nodeConverter;
 
 end
